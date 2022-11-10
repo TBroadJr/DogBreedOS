fd164b1f052f2c21ff11d1ea068eb4a170c0680f
Tornelius <tkbjr28@icloud.com>
2022-11-10 12:27:16 -0500

    Initial Commit

---
+++ DogBreedOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DogBreedOS
+//
+//  Created by Tornelius Broadwater, Jr on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DogBreedOS/DogBreedOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedOSApp.swift
+//  DogBreedOS
+//
+//  Created by Tornelius Broadwater, Jr on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
